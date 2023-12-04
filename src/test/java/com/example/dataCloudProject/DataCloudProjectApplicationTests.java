1e18c6c9596f718bbea9f0b14b018f91200bdf51
alkha <al.khateeb.ahmad96@gmail.com>
2023-12-04 17:42:03 +0100

    Initializing new project

---
+++ src/test/java/com/example/dataCloudProject/DataCloudProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dataCloudProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataCloudProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
